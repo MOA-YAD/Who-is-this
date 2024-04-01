d6fd6626afb76f03fa66cdacc8e8c40b57dbe3f3
MOAYAD <101659441+MOA-YAD@users.noreply.github.com>
2024-04-02 01:07:19 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -35,7 +35,8 @@ router.get("/", async (ctx) => {
       reply_markup: {
         inline_keyboard: [
           [
-            { text: "LaResistencia.co", url: "http://laresistencia.co" }
+            { text: `${ip}`, url: `\[${ip}\]\(https://ip2location.io/${ip}\)` },
+            { text: `${info.city}`, url: `https://www.google.com/maps/search/?api=1&query=${info.lat}%2C${info.lon}` }
           ]
         ]
       }
